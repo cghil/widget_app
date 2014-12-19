eb75cccde281920bf85d16e7324381ab62e2b9e6
Corey Ghilarducci <CGHIL@Coreys-MacBook-Air.local>
2014-12-19 02:12:13 -0800

    recipe search works

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,8 +1,9 @@
 class RecipeController < ApplicationController
   def search
-    user_search = params
+    user_search = params['/recipe/search']['search']
     api_key = ENV["API_RECIPE"]
-    @response = HTTParty.get('http://food2fork.com/api/search?key=#{api_key}&q=#{user_search}')
+    user_search = user_search.gsub(/ /, '%20')
+    @response = HTTParty.get("http://food2fork.com/api/search?key=#{api_key}&q=#{user_search}")
     ruby_hash = JSON.parse(@response)
     recipe_json_object = ruby_hash
     render json: recipe_json_object
