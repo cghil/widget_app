f35cab48e42f2425e19bc1c09d65c8749fd784bd
Corey Ghilarducci <CGHIL@Coreys-MacBook-Air.local>
2014-12-19 17:09:21 -0800

    AJAX'd search results for recipes

---
+++ app/assets/javascripts/recipe.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.search-button').on('click', getNewRecipes)
+});
+
+function getNewRecipes(evt){
+  evt.preventDefault();
+  var recipesearchForm = $('.input-recipe-search').serialize();
+  var recipeAjaxRequest = $.ajax({
+    url: '/recipe/search',
+    type: 'POST',
+    data: recipesearchForm
+  })
+  recipeAjaxRequest.done(getRecipeData)
+}
+
+
+
+function getRecipeData(reply){
+  $('.recipes').remove();
+  var lengthOfRecipes= reply.recipes.length
+  $('h3.recipe-title').after('<div class="recipes"></div>')
+  for (i=0; i<lengthOfRecipes; i++){
+    var html = '<p class="text-center">'+reply.recipes[i].title+'</p><a href='+reply.recipes[i].source_url+'><img src='+reply.recipes[i].image_url+' class="resize thumbnail"></a>'
+    $('.recipes').append(html)
+  }
+}
