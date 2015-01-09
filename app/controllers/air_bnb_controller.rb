c4c04c9eaf6e49986001a764a489c8bc333ffedd
cghil <cghilarducci@gmail.com>
2015-01-09 09:13:40 -0800

    price display fixed + forms

--- app/controllers/air_bnb_controller.rb
+++ app/controllers/air_bnb_controller.rb
@@ -1,4 +1,8 @@
 class AirBnBController < ApplicationController
   def search
+    city = params['/air_bnb/search']['search_city']
+    country = params['/air_bnb/search']['search_country']
+    city.gsub(/ /, "--")!
+    country.gsub(/ /, "--")!
   end
 end
