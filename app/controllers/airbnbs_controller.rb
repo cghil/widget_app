08355fc09d5619747d285faee071c2bac73fc8b4
cghil <cghilarducci@gmail.com>
2015-01-20 21:03:45 -0800

    start ajaxing the search for airbnb rooms

--- app/controllers/airbnbs_controller.rb
+++ app/controllers/airbnbs_controller.rb
@@ -1,5 +1,9 @@
 class AirbnbsController < ApplicationController
   def find
-
+    city = params['/airbnbs/find']['search_city']
+    country = params['/airbnbs/find']['search_country']
+    city = city.gsub(/ /, '--')
+    country = country.gsub(/ /, '--')
+    byebug
   end
 end
