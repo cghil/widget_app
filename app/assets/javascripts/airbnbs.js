a6ad408c30365ef88fac5bf135187450b5dc5d6a
cghil <cghilarducci@gmail.com>
2015-01-21 20:02:42 -0800

    if statements for blank searches

--- app/assets/javascripts/airbnbs.js
+++ app/assets/javascripts/airbnbs.js
@@ -19,10 +19,17 @@ function findRooms(evt){
 
 function getAirbnbData(reply){
   $('.removeable-places').remove();
-  var cityName = reply.city.replace('--', ', ');
-  cityName = toTitleCase(cityName)
-  var title = "<div class='removeable-places'><h2 class='text-center city-name'>"+cityName+"</h2></div>"
-  $('.title-airbnb').after(title)
+  if (reply.city !== ""){
+    var cityName = reply.city.replace('--', ', ');
+    cityName = toTitleCase(cityName)    
+    var title = "<div class='removeable-places'><h2 class='text-center city-name'>"+cityName+"</h2></div>"
+    $('.title-airbnb').after(title)
+  }
+  if (reply.city === ""){
+    cityName = 'You Put A Blank Input In The Search Box'
+    ar title = "<div class='removeable-places'><h2 class='text-center city-name'>"+cityName+"</h2></div>"
+    $('.title-airbnb').after(title)
+  }
   for(i=0; i<18; i++){
     var roomHtml = "<div class='room'><span class='price text-center'><b>$"+reply.prices[i]+"</b></span><a href='https://www.airbnb.com"+reply.urls[i]+"'><img class='resize thumbnail' src='"+reply.photos[i]+"'></a></div>";
     $('.city-name').after(roomHtml);
