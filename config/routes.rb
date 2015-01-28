f59d693c5f27df1820fc9c25b0bedb68a6873847
cghil <cghilarducci@gmail.com>
2015-01-28 11:21:25 -0800

    removed devise from routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   post 'recipe/search' => 'recipe#search'
   # post 'air_bnb/search' => 'air_bnb#search'
   post 'airbnbs/find' => 'airbnbs#find'
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
