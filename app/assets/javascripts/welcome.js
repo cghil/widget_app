42b6ca81b22755a540c731b46a5d347043483fa4
cghil <cghilarducci@gmail.com>
2015-01-27 17:00:24 -0800

    fixed side nave

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -6,7 +6,6 @@ var undoRemove = function(that){
 	var html = '<div id="undo" class="pure-g"><div class="pure-u-1-3"></div> <div class="pure-u-1-3"> <div class="undo text-center black-background white-font opacity-less padding-top-10px"> <div class="inline"><p> Undo the remove of that widget! </p> </div> <i class="fa fa-undo fa-4x"></i> </div> </div> <div class="pure-u-1-3"> </div></div>'
 	$('.widget-holder').prepend(html);
 	$('#undo').fadeOut(3000);
-	$('#undo').hover()
 	var widget = that;
 	$('#undo').click(function(){
 		$('#undo').remove();
@@ -14,7 +13,6 @@ var undoRemove = function(that){
 		$('.close-and-remove').unbind();
 		$('.close-and-remove').on('click', removeWidget);
 	})
-
 };
 
 function removeWidget(){
