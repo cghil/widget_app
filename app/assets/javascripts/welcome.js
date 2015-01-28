705be6ec64399df338bb4e511e9ffc1f1b0b866f
cghil <cghilarducci@gmail.com>
2015-01-27 16:37:07 -0800

    remove and undo remove widget

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,13 +3,22 @@ $(document).ready(function(){
 });
 
 var undoRemove = function(that){
-	var html = '<div class="undo"> <p class="inline"> Undo the remove of that widget! </p> <i class="fa fa-undo fa-4x"></i> </div>'
-	debugger;
+	var html = '<div id="undo" class="pure-g"><div class="pure-u-1-3"></div> <div class="pure-u-1-3"> <div class="undo text-center black-background white-font opacity-less padding-top-10px"> <div class="inline"><p> Undo the remove of that widget! </p> </div> <i class="fa fa-undo fa-4x"></i> </div> </div> <div class="pure-u-1-3"> </div></div>'
+	$('.widget-holder').prepend(html);
+	$('#undo').fadeOut(3000);
+	$('#undo').hover()
+	var widget = that;
+	$('#undo').click(function(){
+		$('#undo').remove();
+		$('.list-widgets').append(widget);
+		$('.close-and-remove').unbind();
+		$('.close-and-remove').on('click', removeWidget);
+	})
+
 };
 
 function removeWidget(){
-	var that = $(this).parent().parent();
-	$(this).parent().parent().remove();
-	undoRemove(that)
-}
-
+	var $that = $(this).parent().parent();
+	$that.remove();
+	undoRemove($that);
+};
