02e756d02ff18159e638a79723a7756c0a44e75c
cghil <cghilarducci@gmail.com>
2015-02-18 16:23:17 -0800

    remove widget from gridster

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,36 @@
 //= require turbolinks
 //= require underscore
 //= require_tree .
+var gridster;
 
 $(function(){
-  var gridster = $(".gridster ul").gridster({
+  gridster = $(".gridster ul").gridster({
     widget_margins: [10, 10],
     widget_base_dimensions: [160, 160]
-  })
+  }).data('gridster');
 });
+
+$(document).ready(function(){
+	$('.close-and-remove').on('click', removeWidget)
+});
+
+var undoRemove = function(that){
+	var html = '<div id="undo" class="pure-g"><div class="pure-u-1-3"></div> <div class="pure-u-1-3"> <div class="undo text-center black-background white-font opacity-less padding-top-10px"> <div class="inline"><p> Undo the remove of that widget! </p> </div> <i class="fa fa-undo fa-4x"></i> </div> </div> <div class="pure-u-1-3"> </div></div>'
+	$('.widget-holder').prepend(html);
+	$('#undo').fadeOut(3000);
+	var widget = that;
+	$('#undo').click(function(){
+		$('#undo').remove();
+		$('.list-widgets').append(widget);
+		$('.close-and-remove').unbind();
+		$('.close-and-remove').on('click', removeWidget);
+	})
+};
+
+
+function removeWidget(){
+	var $that = $(this).parent().parent();
+	gridster.remove_widget($that);
+	var $existingWidgets = $('.list-widgets').children()
+	undoRemove($that);
+};
