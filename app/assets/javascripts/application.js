d7dd70633180d38c4e1d49b1706092889feacee9
cghil <cghilarducci@gmail.com>
2015-02-18 20:27:37 -0800

    removeablity of widgets + sign up stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,25 +28,31 @@ $(function(){
 
 $(document).ready(function(){
 	$('.close-and-remove').on('click', removeWidget)
+	$('#removeallwidgets').on('click', removeAllWidgets)
 });
 
-var undoRemove = function(that){
-	var html = '<div id="undo" class="pure-g"><div class="pure-u-1-3"></div> <div class="pure-u-1-3"> <div class="undo text-center black-background white-font opacity-less padding-top-10px"> <div class="inline"><p> Undo the remove of that widget! </p> </div> <i class="fa fa-undo fa-4x"></i> </div> </div> <div class="pure-u-1-3"> </div></div>'
-	$('.widget-holder').prepend(html);
-	$('#undo').fadeOut(3000);
-	var widget = that;
-	$('#undo').click(function(){
-		$('#undo').remove();
-		$('.list-widgets').append(widget);
-		$('.close-and-remove').unbind();
-		$('.close-and-remove').on('click', removeWidget);
-	})
-};
+function removeAllWidgets(){
+	gridster.remove_all_widgets();
+}
+
+
+// var undoRemove = function(that){
+// 	var html = '<div id="undo" class="pure-g"><div class="pure-u-1-3"></div> <div class="pure-u-1-3"> <div class="undo text-center black-background white-font opacity-less padding-top-10px"> <div class="inline"><p> Undo the remove of that widget! </p> </div> <i class="fa fa-undo fa-4x"></i> </div> </div> <div class="pure-u-1-3"> </div></div>'
+// 	$('.widget-holder').prepend(html);
+// 	$('#undo').fadeOut(3000);
+// 	var widget = that;
+// 	$('#undo').click(function(){
+// 		$('#undo').remove();
+// 		$('.list-widgets').append(widget);
+// 		$('.close-and-remove').unbind();
+// 		$('.close-and-remove').on('click', removeWidget);
+// 	})
+// };
 
 
 function removeWidget(){
 	var $that = $(this).parent().parent();
 	gridster.remove_widget($that);
 	var $existingWidgets = $('.list-widgets').children()
-	undoRemove($that);
+	// undoRemove($that);
 };
